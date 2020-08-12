0fc223d6f46f6c457b5b9a82b8290304f4706774
abrar-nazib <69598716+abrar-nazib@users.noreply.github.com>
2020-08-13 00:04:39 +0600

    Create modules.js

---
+++ modules.js
@@ -0,0 +1,11 @@
+//All modules are listed here
+
+
+//----- Filesystem ------------------------------------------
+const fileSystem = require('fs');  //fs stands for filesystem
+fs.readdirSync('filename'); // reads directory synchronously -> shows the contents of a directory
+
+
+//------Operating system related module--------------------
+const os = require('os');  // os stands for Operating system
+
