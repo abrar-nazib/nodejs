1dbceab514db174ff483e1f68a6f4ef7812c8f15
abrar-nazib <69598716+abrar-nazib@users.noreply.github.com>
2020-08-13 00:08:26 +0600

    Update modules.js

--- modules.js
+++ modules.js
@@ -4,7 +4,7 @@
 //----- Filesystem ------------------------------------------
 const fileSystem = require('fs');  //fs stands for filesystem
 fs.readdirSync('filename'); // reads directory synchronously -> shows the contents of a directory
-
+fs.readdir('filename', function(err, files){  /*handle the errors or handle the files array*/ }); //second parameter is a callback function
 
 //------Operating system related module--------------------
 const os = require('os');  // os stands for Operating system
