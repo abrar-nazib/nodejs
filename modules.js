2fda2b6265b259a8a1520e90798e9ba3e2f29089
abrar <abrarnazib@gmail.com>
2020-08-13 13:21:25 +0600

    last change in august 13 2020

--- modules.js
+++ modules.js
@@ -1,11 +1,43 @@
-//All modules are listed here
+//----- Filesystem -------------------------------------------------------------------------------------------------
+const fileSystem = require('fs');
+//fs stands for filesystem
+
+fs.readdirSync('filename');
+// reads directory synchronously -> shows the contents of a directory
+
+fs.readdir('filename', function (err, files) {
+    /*handle the errors or handle the files array*/
+});
+//second parameter is a callback function
 
 
-//----- Filesystem ------------------------------------------
-const fileSystem = require('fs');  //fs stands for filesystem
-fs.readdirSync('filename'); // reads directory synchronously -> shows the contents of a directory
-fs.readdir('filename', function(err, files){  /*handle the errors or handle the files array*/ }); //second parameter is a callback function
 
-//------Operating system related module--------------------
-const os = require('os');  // os stands for Operating system
 
+//------Operating system related module-----------------------------------------------------------------------------------
+const os = require('os');
+// os stands for Operating system
+
+os.freemem();
+// returns free memory space in memory
+
+os.totalmem();
+// returns total memoru space in memory
+
+
+
+
+//-----Events----------------------------------------------------------------------------------------------------------
+const EventEmitter = require("events");
+//it is a class and class names should be written in this conversion(first capital)
+
+const emitter = new EventEmitter();
+//module we need
+
+emitter.on("eventName", function (arg) {});
+//event listener -> will be called while the event is emitted
+//arg refers to the properties sent by the event
+
+emitter.emit("eventName", {
+    /* properties to be passed as object*/
+});
+//emits an event #emitter must be called after listener is defined -> asynschronous stuff
